bc22d35ac98d5a27239153846d78a45a944804bf
paria <pariyataba@gmail.com>
2026-02-12 12:46:07 +0100

    complete contain in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,38 @@
 import Header from "../components/Header"
 import "../pages/css/home.css"
+import pic1 from "../../public/1.jpeg"
+import pic2 from "../../public/2.jpeg"
+import pic3 from "../../public/3.jpeg"
+import Footer from "../components/Footer.tsx"
 function Home(){
 	return(
 		<div>
-			<Header></Header>
-			Home page
+		<Header></Header>
+		<div className="title-div">
+		<h1>Mary 7</h1>
+		<h2>Skönhet & Välbefinnande</h2>
 		</div>
 
+		<div className="home-main-div">
+			<div className="div-image">
+				<img src={pic1} alt="Bild av skönhetssalongen" />
+				<img src={pic2} alt="Bild av skönhetssalongen" />
+				<img src={pic3} alt="Bild av skönhetssalongen" />
+			</div>
+			<div className="div-main">
+				<p>Med många års erfarenhet skapar vi skönhet med passion och precision</p>
+				<button>BOKA TID HÄR</button>
+			</div>
+
+		</div>
+		<div className="question-div">
+			<p className="p-1">Har du en fråga?</p>
+			<p className="p-2">Skicka oss ett meddelande</p>
+		</div>
+		<Footer></Footer>
+		
+		</div>
+		
 	)
 }
 export default Home
