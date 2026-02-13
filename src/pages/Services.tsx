6a7df9b573d626167299eaa046acf22a50325d7d
paria <pariyataba@gmail.com>
2026-02-13 12:50:11 +0100

    add responsive in servisec page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -5,36 +5,44 @@ import Footer from "../components/Footer"
 import { NavLink } from "react-router-dom"
 
 function Services() {
-  const [behandlingar, setBehandlingar] = useState([])
-
-  useEffect(() => {
-    fetch("/api/behandlingar")
-      .then(res => res.json())
-      .then(data => setBehandlingar(data))
-  }, [])
-
-  return (
-    <div>
-      <Header />
-      <h1>Behandlingar</h1>
-
-      <div className="services-grid">
-        {behandlingar.map((item: any) => (
-          <div key={item._id} className="service-card">
-            <img src={item.icon} alt={item.title} />
-            <h2>{item.title}</h2>
-            <p>{item.description}</p>
-
-            <NavLink to={`/behandlingar/${item._id}`} className="more-btn">
-              Mer
-            </NavLink>
-          </div>
-        ))}
-      </div>
-
-      <Footer />
-    </div>
-  )
+	const [behandlingar, setBehandlingar] = useState([])
+	
+	useEffect(() => {
+		fetch("/api/behandlingar")
+		.then(res => res.json())
+		.then(data => setBehandlingar(data))
+	}, [])
+	
+	return (
+		<div>
+		<Header />
+		<h1>Behandlingar</h1>
+		
+		<div className="services-grid">
+		{behandlingar.map((item: any) => (
+			<div key={item._id} className="service-card">
+			<div className="card-row">
+			<div> <img src={item.icon} alt={item.title} /></div>
+			
+			<div className="card-text"> 
+				 <p className="card-title">{item.title}</p>  
+				  <p className="card-des">{item.description}</p>
+			</div>
+			
+			</div>
+			<div className="div-button">
+				<NavLink to={`/behandlingar/${item._id}`} className="more-btn">
+			Mer
+			</NavLink>
+			</div>
+			
+			</div>
+		))}
+		</div>
+		
+		<Footer />
+		</div>
+	)
 }
 
 export default Services
