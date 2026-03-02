af6ed245106526a2447da57b10846897eb02a863
paria <pariyataba@gmail.com>
2026-03-02 14:27:25 +0100

    solve problem for deploying

--- srcServer/server.ts
+++ srcServer/server.ts
@@ -5,6 +5,9 @@ import express from "express"
 import cors from "cors"
 import http from "http"
 import { Server } from "socket.io"
+import path from "path"
+import { fileURLToPath } from "url"
+
 import { connectDB } from "./config/db.js"
 import serviceRoutes from "./routes/behandling.routes.js"
 import adminRoutes from "./routes/admin.routes.js"
@@ -15,15 +18,18 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const port: number = Number(process.env.PORT) || 1337
+// FIX __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
+// Correct PORT for Render
+const port: number = Number(process.env.PORT) || 1337
 
 const server = http.createServer(app)
 
-
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*", 
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 })
@@ -34,10 +40,23 @@ io.on("connection", (socket) => {
 
 connectDB()
 
-app.use(express.static("./dist"))
+//  FIX STATIC PATH 
+app.use(
+  express.static(
+    path.join(__dirname, "../../dist")
+  )
+)
+
+//  API routes
 app.use("/api/admin", adminRoutes)
 app.use("/api/behandlingar", serviceRoutes)
 app.use("/api", messageRoute)
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../../dist/index.html")
+  )
+})
 
 server.listen(port, () => {
   console.log("Server running on port " + port)
